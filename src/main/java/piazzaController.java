05ee5091d9ad142996ce8723f931aa2354d7375c
Bendik Holter <bendikholter@Bendiks-MacBook-Pro.local>
2021-03-21 11:58:31 +0100

    La til funksjon for å hente statistikk #5

--- src/main/java/piazzaController.java
+++ src/main/java/piazzaController.java
@@ -49,16 +49,23 @@ public class piazzaController extends DBConn {
 		throw new IllegalStateException("Du er ikke logget inn");
 	}
 	
+	/**
+	 * Hjelpemetode som sjekker om man er logget inn.
+	 * */
+	private boolean isLoggetIn() {
+		return this.email != null;
+	}
+	
 
 	/**
 	 * Hjelpemetode som registrerer en post i databasen. Dvs et reply eller en thread(Den første posten).
 	 * */
 	private boolean makePost(int threadID, String content, String postName,  String folderName, String courseID, int courseYear, String tag, boolean anonymous) {
-		if(this.email == null) {
+		if(!isLoggetIn()) {
 			throw new IllegalStateException("Du må være logget inn for å poste.");
 		}
 		try {
-			// Sjekker om brukeren er oppmeldt i emnet og om man har lov å poste anonymt i kurset som hører til folderen
+			// Henter ut om man har lov å poste anonymt i emnet
 			PreparedStatement statement = this.conn.prepareStatement("select allowAnonumous from user inner join memberOfCourse on user.email = memberOfCourse.email \n" + 
 					"	inner join courseInYear on memberOfCourse.courseID = courseInYear.courseID\n" + 
 					"	where user.email = (?) and courseInYear.courseID = (?) and courseInYear.courseYear = (?)");
@@ -67,12 +74,13 @@ public class piazzaController extends DBConn {
 			statement.setInt(3, courseYear);
 			ResultSet rs = statement.executeQuery();
 			boolean anonymousAllowed = false;
+			// Sjekker om brukeren er oppmeldt i emnet 
 			if(!rs.next()) {
 				throw new IllegalArgumentException("Du er ikke oppmeldt i dette emnet.");
 			}
 			anonymousAllowed = rs.getBoolean("allowAnonumous");
 		
-			// Setter inn posten
+			// Lagrer posten i databasen
 			statement = this.conn.prepareStatement("INSERT INTO post VALUES ( (?), (?), (?), (?), (?), (?), (?), (?))");
 			this.totalPosts += 1;
 			statement.setInt(1, this.totalPosts);
@@ -138,6 +146,9 @@ public class piazzaController extends DBConn {
 	 * Metode som lager et nytt reply til en tidligere thread.
 	 * */
 	public boolean replyTo(int postID, String replyText, String replyName, boolean isAnonymous) {
+		if(!isLoggetIn()) {
+			throw new IllegalStateException("Du må være logget inn for å svare på en post.");
+		}
 		try {
 			// Vi må først finne threadID til posten vi skal svare på
 			PreparedStatement statement = this.conn.prepareStatement(" select threadID from post\n" + 
@@ -148,7 +159,7 @@ public class piazzaController extends DBConn {
 				throw new IllegalArgumentException("Det finnes ikke post med denne postID-en");
 			}
 			int threadID = rs.getInt("threadID");
-			// Henter folderName og courseName til posten vi svarer på
+			// Henter folderName, courseId og courseYear til posten vi svarer på
 			statement = this.conn.prepareStatement("select folderName, courseInYear.courseID, courseInYear.courseYear\n" +
 					" from post inner join threadInFolder on threadInFolder.postID = post.postID\n" + 
 					" inner join courseInYear on courseInYear.courseID = threadInFolder.folderCourseID \n" + 
@@ -178,8 +189,8 @@ public class piazzaController extends DBConn {
 	 * */
 	public Collection<Integer> searchFor(String content, String courseID, int year) {
 		// Sjekker om brukeren er innlogget
-		if(this.email == null) {
-			throw new IllegalStateException("Du må være logget inn for å poste.");
+		if(!isLoggetIn()) {
+			throw new IllegalStateException("Du må være logget inn for å søke etter innhold.");
 		}
 		Collection<Integer> lst = new ArrayList<Integer>();
 		// Sjekker at brukeren er medlem av emnet dette året.
@@ -192,7 +203,7 @@ public class piazzaController extends DBConn {
 			if(!rs.next()) {
 				throw new IllegalArgumentException("Du er ikke oppmeldt i dette faget.");
 			}
-			// Finner alle posts i dette emnet(courseInYear) som inneholder content
+			// Finner alle posts i dette emnet(courseInYear) som inneholder content. (Enten i navnet på posten eller i innholdet)
 			statement = this.conn.prepareStatement("select post.postId from (select post.postID, courseInYear.courseID, courseInYear.courseYear from courseInYear inner join folder on folder.courseID = courseInYear.courseID\n" + 
 					"	and folder.folderYear = courseInYear.courseYear\n" + 
 					"	inner join threadInFolder on threadInFolder.folderCourseID = folder.courseID\n" + 
@@ -212,12 +223,104 @@ public class piazzaController extends DBConn {
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new RuntimeException("Det oppsto en feil i searchFor", e);
 		}
 		return lst;
 	}
+	
+	/**
+	 * Hjelpeklasse som brukes for å lagre statistikk om en bruker
+	 * */
+	public static class UserStatistics {
+		private String email;
+		private int antallSett;
+		private int antallLaget;
+		
+		public UserStatistics(String email, int antallSett, int antallLaget) {
+			this.email = email;
+			this.antallSett = antallSett;
+			this.antallLaget = antallLaget;
+		}
+		
+		public String getEmail() {
+			return this.email;
+		}
+		
+		public int getAntallSett() {
+			return this.antallSett;
+		}
+		
+		public int getAntallLaget() {
+			return this.antallLaget;
+		}
+		
+		public String toString() {
+			return this.email + " har sett " + this.antallSett + " og har laget " + this.antallLaget + " poster";
+		}
+		
+	}
+	
+	
+	/**
+	 * Metode som henter statistikk for alle brukere som er medlem av emnet for det gitte året.
+	 * */
+	public List<UserStatistics> getStatistics(String courseID, int courseYear) {
+		// Skal sjekke om brukeren er en instruktør
+		if(!isLoggetIn()) {
+			throw new IllegalStateException("Du må være logget inn for å søke etter innhold.");
+		}
+		List<UserStatistics> statistics = new ArrayList<piazzaController.UserStatistics>();
+		PreparedStatement statement;
+		try {
+			// Spøring som finner ut om brukeren er instuktør i det gitte emnet.
+			statement = this.conn.prepareStatement("select isInstructor from user inner join memberOfCourse on memberOfCourse.email = user.email\n" +
+					"    where memberofcourse.email = (?) and memberOfCourse.courseID = (?) and memberOfCourse.mocYear = (?)");
+			statement.setString(1, this.email);
+			statement.setString(2, courseID);
+			statement.setInt(3, courseYear);
+			ResultSet rs = statement.executeQuery();
+			boolean isInstructor = false;
+			if (!rs.next()) {
+				throw new IllegalArgumentException("Du er ikke medlem av dette emnet");
+			}
+			isInstructor = rs.getBoolean("isInstructor");
+			System.out.println(isInstructor);
+			if(!isInstructor) {
+				throw new IllegalStateException("Du må være en instuktør i emnet for å kunne se statistikk");
+			}
+			// Spørring som henter statistikk om alle brukerene som er medlem av det gitte emnet.
+			statement = this.conn.prepareStatement("select A.email, antallSett, antallLaget from (select user.email as email, count(viewedPost.email) as antallSett from user left join viewedPost on viewedPost.email = user.email\n" + 
+					"where user.email in (select user.email from user inner join memberOfCourse on memberOfCourse.email = user.email\n" + 
+					"where memberOfCourse.courseID = (?) and memberOfCourse.mocYear = (?) )\n" + 
+					"group by user.email) as A\n" + 
+					"left join (select user.email as email, count(post.userEmail) as antallLaget from user left join post on post.userEmail = user.email\n" + 
+					"where post.postID in (select post.postID from post inner join threadInFolder on threadInFolder.postID = post.postID\n" + 
+					"inner join folder on folder.courseID = threadInFolder.folderCourseID and folder.folderYear = threadInFolder.folderCourseYear\n" + 
+					"and folder.folderName = threadInFolder.folderName\n" + 
+					"where folder.courseID = (?) and folder.folderYear = (?) )\n" + 
+					"group by user.email) as B on A.email = B.email\n" + 
+					"order by antallSett desc");
+			statement.setString(1, courseID);
+			statement.setInt(2, courseYear);
+			statement.setString(3, courseID);
+			statement.setInt(4, courseYear);
+			rs = statement.executeQuery();
+			// Lager en liste med statistikk for hver bruker
+			while(rs.next()) {
+				String email = rs.getString("email");
+				int antallSett = rs.getInt("antallSett");
+				int antallLaget = rs.getInt("antallLaget");
+				statistics.add(new UserStatistics(email, antallSett, antallLaget));
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException("Det oppsto en feil under henting av statistikk", e);
+		}
+		return statistics;
+	}
+	
 
 	public static void main(String[] args) {
+		// Husk å kjøre setUp filen først.
 		piazzaController controller = new piazzaController();
 		controller.logInUser("bendik@gmail.com", "bendik");
 		controller.post("min første post", "første", "eksamen", "TDT4100", 2021, "question", true);
@@ -227,35 +330,16 @@ public class piazzaController extends DBConn {
 		controller.post("min andre post", "post", "eksamen", "TDT4145", 2021, false);
 		controller.logOutUser();
 		controller.logInUser("jarl@gmail.com", "jarl");
+		controller.post("min første post", "første", "eksamen", "TDT4100", 2021, "question", true);
 		controller.replyTo(1, "hei", "første reply", true);
+		
 		// Spør etter postene som inneholder "post"
 		Collection<Integer> lst = controller.searchFor("post", "TDT4100", 2021);
 		System.out.println("Disse postene inneholder post:");
 		System.out.println(lst);
+		
+		// Henter statistikk
+		List<piazzaController.UserStatistics> lst1 = controller.getStatistics("TDT4100", 2021);
+		System.out.println(lst1);
 	}
-	
-
-//# insert into user values("bendik@gmail.com", "bendik", "bendik");
-//-- insert into course values(1, "TDT4100", "vår");
-//-- insert into courseInYear values(1, 2021, true); 
-//-- insert into memberOfCourse values("bendik@gmail.com", 1, 2021, false);
-//-- insert into folder values(1, 2021, "eksamen");
-
-	// Skal sjekke om brukeren er en instruktør
-	/*PreparedStatement statement = this.conn.prepareStatement("select isInstructor from post \n" +
-			"\tinner join threadinfolder on post.threadID = threadinfolder.postID\n" +
-			"    inner join courseinyear on folderCourseID = courseinyear.courseID\n" +
-			"    inner join memberofcourse on courseinyear.courseID = memberofcourse.courseID\n" +
-			"    where memberofcourse.email = (?) and post.postID = (?)");
-			statement.setString(1, "jarl@gmail.com");
-			statement.setInt(2, postID);
-	ResultSet rs = statement.executeQuery();
-	boolean isInstructor = false;
-			if (rs.next()) {
-		isInstructor = rs.getBoolean("isInstructor");
-		System.out.println(isInstructor);
-	}
-			else {
-		throw new RuntimeException("Det oppsto en feil i sjekking om brukeren er en instruktør ");
-	}*/
 }
